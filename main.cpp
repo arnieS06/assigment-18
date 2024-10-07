7039f426eccbd724a3331562aca388e27384697e
arnieS06 <sharma.arnav2016@gmail.com>
2024-10-07 11:07:16 -0700

    updated

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
 
+void bubbleSort(int arrayOfNumbers[], int numberOfIntegers);
+
+int main() {
+
+    int numberOfIntegers;
+    cout << "how many numbers do you want to give me? -> ";
+    cin >> numberOfIntegers;
+
+    int arrayOfNumbers[numberOfIntegers];
+
+    for (int i = 0; i < numberOfIntegers; i++) {
+        cout << "Enter your number: ";
+        cin >> arrayOfNumbers[i];
+    }
+
+
+}
+
+void bubbleSort(int arrayOfNumbers[], int numberOfIntegers) {
+    for (int i = 0; i < numberOfIntegers; i++) {
+        if (arrayOfNumbers[i] > arrayOfNumbers[i + 1]) {
+            
+
+
+        }
+    }
+}
